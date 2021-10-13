7de7ecb5f0e6c9d63e1b61a3bb9124672658da1b
HENRY-KC <henrykc24@yahoo.com>
2021-10-13 06:22:46 +0100

    Add logic for filtering states in the reducer

--- src/redux/covid19/covid19.js
+++ src/redux/covid19/covid19.js
@@ -2,10 +2,16 @@ import initialState from '../initialState';
 
 // Constants
 const FETCH_DATA = 'covid19_info_finder/covid19/FETCH_DATA';
+const FILTER_STATE = 'covid19_info_finder/covid19/FILTER_STATE';
 
 // Action Creators
 export const fetchData = (payload) => ({
   type: FETCH_DATA,
+  payload,
+});
+
+export const filterState = (payload) => ({
+  type: FILTER_STATE,
   payload,
 });
 
@@ -16,29 +22,52 @@ const covid19Reducer = (state = initialState, action) => {
     case FETCH_DATA:
       return payload;
 
+    case FILTER_STATE:
+      return payload;
+
     default:
       return state;
   }
 };
 
-export const getCovidInfoFromServer = () => async (dispatch) => {
-  const now = new Date();
-  const dateObject = {
-    year: now.getFullYear(),
-    month: now.getMonth(),
-    date: now.getDate(),
-  };
+export const getCovidInfoFromServer = (dateValue) => async (dispatch) => {
+  try {
+    const now = new Date();
+    const dateObject = {
+      year: now.getFullYear(),
+      month: now.getMonth() + 1,
+      date: now.getDate(),
+    };
 
-  const { year } = dateObject;
-  const month = dateObject.month.toString().length === 1 ? `0${dateObject.month}` : dateObject.month;
-  const date = dateObject.date.toString().length === 1 ? `0${dateObject.date}` : dateObject.date;
-  const url = `https://api.covid19tracking.narrativa.com/api/${year}-${month}-${date}`;
-  const tempResult = await fetch(url);
-  const finalResult = await tempResult.json();
-  const dateString = `${year}-${month}-${date}`;
-  const countriesDataArray = Object.values(finalResult.dates[dateString].countries);
+    const { year } = dateObject;
+    const month = dateObject.month.toString().length === 1 ? `0${dateObject.month}` : dateObject.month;
+    const date = dateObject.date.toString().length === 1 ? `0${dateObject.date}` : dateObject.date;
+    const defaultDate = `${year}-${month}-${date}`;
+    const url = `https://api.covid19tracking.narrativa.com/api/${dateValue || defaultDate}`;
+    const tempResult = await fetch(url);
+    const finalResult = await tempResult.json();
+    // const dateString = `${year}-${month}-${date}`;
+    const countriesDataArray = Object.values(finalResult.dates[dateValue || defaultDate].countries);
+    dispatch(fetchData(countriesDataArray));
+  } catch (e) {
+    const now = new Date();
+    const dateObject = {
+      year: now.getFullYear(),
+      month: now.getMonth() + 1,
+      date: now.getDate() - 1,
+    };
 
-  dispatch(fetchData(countriesDataArray));
+    const { year } = dateObject;
+    const month = dateObject.month.toString().length === 1 ? `0${dateObject.month}` : dateObject.month;
+    const date = dateObject.date.toString().length === 1 ? `0${dateObject.date}` : dateObject.date;
+    const defaultDate = `${year}-${month}-${date}`;
+    const url = `https://api.covid19tracking.narrativa.com/api/${dateValue || defaultDate}`;
+    const tempResult = await fetch(url);
+    const finalResult = await tempResult.json();
+    // const dateString = `${year}-${month}-${date}`;
+    const countriesDataArray = Object.values(finalResult.dates[dateValue || defaultDate].countries);
+    dispatch(fetchData(countriesDataArray));
+  }
 };
 
 export default covid19Reducer;
