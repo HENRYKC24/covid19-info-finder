c2d48f0a5f4eddff3ba20b6d23d9a7cd1143c092
HENRY-KC <henrykc24@yahoo.com>
2021-10-14 15:25:11 +0100

    Add flag reducer to the store configuration

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,8 +2,12 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import covid19Reducer from './covid19/covid19';
+import covid19FlagsReducer from './covid19/flags';
 
-const rootReducer = combineReducers({ covid19Data: covid19Reducer });
+const rootReducer = combineReducers({
+  covid19Data: covid19Reducer,
+  nationalFlags: covid19FlagsReducer,
+});
 
 const store = createStore(rootReducer, applyMiddleware(logger, thunk));
 
